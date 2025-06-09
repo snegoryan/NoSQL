7e12a3074ac318da69e7469059feee8878c5f955
snegoryan <snegovoyis@gmail.com>
2025-06-09 11:39:31 +0300

    Create otus-py-producer.py

---
+++ 12_kafka_files/otus-py-producer.py
@@ -0,0 +1,21 @@
+from confluent_kafka import Producer
+
+conf = {
+    'bootstrap.servers': 'localhost:9091'
+}
+
+producer = Producer(conf)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Ошибка доставки сообщения: {err}')
+    else:
+        print(f'Сообщение доставлено в {msg.topic()} [{msg.partition()}]')
+
+topic = 'my-topic'
+
+for i in range(10):
+    producer.produce(topic, key=str(i), value=f'Сообщение {i}', callback=delivery_report)
+    producer.poll(0)
+
+producer.flush()
